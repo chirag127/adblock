715e89b918bb19d7e3b46cdb0fc0272a31b0b708
chirag <whyiswhen@gmail.com>
2021-11-19 17:47:29 +0530

    done

--- sortallfiles.py
+++ sortallfiles.py
@@ -6,10 +6,27 @@
 # write code to a list of urls in the files in the and sort them alphabetically and overwrite the original files
 #  with the sorted list
 
+import os
+
+# define the function to LIST ALL THE FILES IN THE current DIRECTORY AND SUBDIRECTORIES 
+def list_all_files():
+    # get the current directory
+    current_dir = os.getcwd()
+    # get the list of all files in the current directory
+    files = os.listdir(current_dir)
+    # get the list of all files in the current directory and subdirectories
+    files_all = [os.path.join(current_dir, file) for file in files]
+    # get the list of all files in the current directory and subdirectories
+    files_all = [file for file in files_all if os.path.isfile(file)]
+    return files_all
+
+    
+
+
+
 
 # list all files in the current directory and directories within it
-files = ['AD.txt', 'AL.txt', 'D.txt', 'E.txt', 'F.txt', 'FE.txt', 'N.txt', 'O.txt', 'P.txt', 'S.txt', 'T.txt', 'WL.txt' , 'UBOS/AD.txt', 'UBOS/AL.txt', 'UBOS/D.txt', 'UBOS/E.txt', 'UBOS/F.txt', 'UBOS/FE.txt', 'UBOS/N.txt', 'UBOS/O.txt', 'UBOS/P.txt', 'UBOS/S.txt', 'UBOS/T.txt', 'UBOS/WL.txt']
-
+files = ['AD.txt', 'AL.txt', 'D.txt', 'E.txt', 'F.txt', 'FE.txt', 'N.txt', 'O.txt', 'P.txt', 'S.txt', 'T.txt', 'WL.txt' , 'UBOS/AD.txt' , 'UBOS/AAll.txt']
 
 
 # define the function to convert the contents of the files to a list of strings
@@ -19,6 +36,10 @@ def convert_to_list(filename):
         content = file.readlines()
     return content
 
+# remove empty lines from the list of strings
+def remove_empty_lines(list):
+    return [line for line in list if line.strip()]
+    
 # define the function to sort the list of strings
 def sort_list(list):
     return sorted(list)
@@ -35,14 +56,18 @@ if __name__ == '__main__':
     # loop through the list of files
     for file in files:
 
-        # convert the file contents to a list of strings
-        file_list = convert_to_list(file)
+        # convert the contents of the files to a list of strings
+        content = convert_to_list(file)
+
+        # remove empty lines from the list of strings
+        content = remove_empty_lines(content)
 
         # sort the list of strings
-        sorted_list = sort_list(file_list)
+        content = sort_list(content)
 
-        # write the sorted list to the file
-        write_to_file(file, sorted_list)
+        # write the sorted list to the files
+
+        write_to_file(file, content)
 
     print('Done')
 
