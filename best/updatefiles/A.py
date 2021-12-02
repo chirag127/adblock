405e1f02b64f9ae41be53bbe3157fb7b03fd4421
chirag <whyiswhen@gmail.com>
2021-12-02 21:38:45 +0530

    done

--- best/updatefiles/A.py
+++ best/updatefiles/A.py
@@ -1,5 +1,19 @@
 # add all txt files to the current directory
+
 import os
+
+files = []
+
+#  Add all files to a list
+for dir in os.listdir('.'):
+
+    if dir.endswith('.txt'):
+        files.append(dir)
+
+
+# remove NF.txt from the list
+files.remove('A.txt')
+
 
 file = open('A.txt', 'w')
 
@@ -9,7 +23,7 @@ file.write("""! Description: It contains all list
 ! Title: Chirag's All Annoyances list
 """)
 
-for dir in os.listdir('.'):
+for dir in files:
 
     if dir.endswith('.txt'):
 
