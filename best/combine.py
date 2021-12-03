9fcd0171cbe0debdd67e4b6392b2ad84494676ea
chirag <whyiswhen@gmail.com>
2021-12-03 21:27:33 +0530

    Update combine.py

--- best/combine.py
+++ best/combine.py
@@ -2,7 +2,5 @@ import glob
 
 read_files = glob.glob("*.txt")
 
-with open("result.txt", "wb") as outfile:
-    for f in read_files:
-        with open(f, "rb") as infile:
-            outfile.write(infile.read())
+for f in read_files:
+    print(f)
