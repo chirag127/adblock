9bd3fb59da69bf18e89a9a78b4429cfc61c5c453
chirag <whyiswhen@gmail.com>
2021-12-03 18:17:37 +0530

    update a.txt

--- best/files.py
+++ best/files.py
@@ -4,19 +4,18 @@ import os
 # define the function to LIST ALL THE FILES IN THE current DIRECTORY AND SUBDIRECTORIES
 
 
-def list_all_files():
-    # get the current directory
-    current_dir = os.getcwd()
-    # get the list of all files in the current directory
-    files = os.listdir(current_dir)
-    # get the list of all files in the current directory and subdirectories
-    files_all = [os.path.join(current_dir, file) for file in files]
-    # get the list of all files in the current directory and subdirectories
-    files_all = [file for file in files_all if os.path.isfile(file)]
 
-    print(files_all.sort())
-    
+import os
 
-list_all_files()
+files = []
+
+#  Add all files to a list
+for dir in os.listdir('.'):
+
+    if dir.endswith('.txt'):
+        files.append(dir)
+        print(dir)
 
 
+#  print number of items in the list
+print(len(files))
