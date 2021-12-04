7dfb1ce9ddd70441c48621495cf86842d4bb7d86
chirag <whyiswhen@gmail.com>
2021-12-04 21:14:44 +0530

    email to mail

---
+++ best/sitespecific/ublock.py
@@ -0,0 +1,34 @@
+import requests
+
+
+urls = """https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt
+https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
+https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
+https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
+https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resourceabuse.txt
+https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt
+https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2020.txt
+https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2021.txt
+https://easylist.to/
+https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_English/filter.txt
+https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt
+https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_17_TrackParam/filter.txt
+https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_4_Social/filter.txt
+https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt"""
+
+urls_list = urls.split()
+
+domain = "youtube.com"
+
+
+for url in urls_list:
+    response = requests.get(url)
+    if response.status_code == 200:
+        rules_list = response.text.split('\n')
+        for rule in rules_list:
+            # write rule to sitespecific/domain.txt if it contains domain
+            if domain in rule:
+                f = open(f"best/sitespecific/{domain}.txt", "a")
+                f.write(rule + '\n' + "filterlist = " + url + '\n' )
+
+print("Done!")
