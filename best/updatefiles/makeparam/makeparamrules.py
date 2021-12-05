ae47ee64139738895b0df077b8ebe1b85c2f4807
chirag <whyiswhen@gmail.com>
2021-12-05 21:47:30 +0530

    best/updatefiles/makeparam:

---
+++ best/updatefiles/makeparam/makeparamrules.py
@@ -0,0 +1,18 @@
+
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+
+file = open("best/updatefiles/makeparam/url.txt", "r")
+
+
+for url in file:
+    url = url.strip()
+    domain = urlparse(url).netloc
+    parsed_url = urlparse(url)
+    query = parse_qs(parsed_url.query)
+    for key, value in query.items():
+        with open("P.txt", "a") as f:
+            f.write(f"||{domain}^$removeparam={key}\n")
+
+with open("best/updatefiles/makeparam/url.txt", "w") as f:
+    f.write("")
