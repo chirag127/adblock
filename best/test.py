59d2f510e3a96d2d285e6288fa3776077875b1d0
chirag <whyiswhen@gmail.com>
2021-12-12 14:20:50 +0530

    sync

--- best/test.py
+++
@@ -1,3 +0,0 @@
-# You are given a rectangle in a plane. The coordinates of one of its diagonals are provided to you. You have to print the total area of the rectangle.
-# The coordinates of the rectangle are provided as four integral values: x1, y1, x2, y2. It is given that x1 < x2 and y1 < y2.
-
