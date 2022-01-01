8d9491b909dd3f12953b75e5fb9f1085d488bc87
chirag <whyiswhen@gmail.com>
2022-01-01 20:15:30 +0530

    host python

---
+++ host/hp/hp.py
@@ -0,0 +1,11 @@
+with open("host\\hp\\raw.txt","r") as f:
+    lines = f.readlines()
+    for line in lines:
+        with open("host\\hp\\hp.txt","a") as f1:
+            f1.write(f"0.0.0.0 {line}")
+            f1.write("\n")
+
+# empty the file
+with open("host\\hp\\raw.txt","w") as f:
+    f.write("")
+
