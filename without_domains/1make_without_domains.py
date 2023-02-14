efc21ae2623b3fb3b879f2812515636717ed01ed
Chirag <76880977+chirag127@users.noreply.github.com>
2023-02-14 14:42:13 +0000

    make it chunks

--- without_domains/1make_without_domains.py
+++ without_domains/1make_without_domains.py
@@ -1,45 +1,68 @@
+from typing import List
 import requests
 
-adguard_registry = (
-    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/"
-)
-urls = {
-    "AdGuard tracking": adguard_registry + "filter_3_Spyware/filter.txt",
-    "AdGuard social": adguard_registry + "filter_4_Social/filter.txt",
-    "AdGuard annoyances": adguard_registry + "filter_14_Annoyances/filter.txt",
-    "easyprivacy": "https://filters.adtidy.org/extension/ublock"
-    + "/filters/118_optimized.txt",
-}
-
-
-for name, url in urls.items():
+def get_rules(url):
     response = requests.get(url)
     if response.status_code == 200:
-        rules_list = response.text.splitlines()
+        return response.text.splitlines()
+    else:
+        raise Exception("Something went wrong while getting the rules")
 
-        new_rules_list = []
-        for rule in rules_list:
+def remove_domains(rules_list: List[str]) -> List[str]:
+    new_rules_list = []
+    for rule in rules_list:
+        if (
+            rule.startswith("||")
+            and (rule.endswith("^") or rule.endswith("^$third-party"))
+            or rule.startswith("!")
+        ):
+            pass
+        else:
+            new_rules_list.append(rule)
+    return new_rules_list
 
-            # check if the rule is a domain
-            if (
-                rule.startswith("||")
-                and (rule.endswith("^") or rule.endswith("^$third-party"))
-                or rule.startswith("!")
-            ):
+def remove_domains(rules_list: List[str]) -> List[str]:
+    new_rules_list = []
+    for rule in rules_list:
+        if (
+            rule.startswith("||")
+            and (rule.endswith("^") or rule.endswith("^$third-party"))
+            or rule.startswith("!")
+        ):
+            pass
+        else:
+            new_rules_list.append(rule)
+    return new_rules_list
 
-                pass
+def remove_duplicates(rules_list):
+    return list(set(rules_list))
 
-            else:
+def sort_list(rules_list):
+    rules_list.sort()
+    return rules_list
 
-                new_rules_list.append(rule)
+def save_rules(name, rules_list):
+    with open(f"without_domains/{name}.txt", "w", encoding="utf8") as f:
+        f.write("\n".join(rules_list))
 
-        # remove duplicates
-        new_rules_list = list(set(new_rules_list))
+def main():
+    adguard_registry = (
+        "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/"
+    )
+    urls = {
+        "AdGuard tracking": adguard_registry + "filter_3_Spyware/filter.txt",
+        "AdGuard social": adguard_registry + "filter_4_Social/filter.txt",
+        "AdGuard annoyances": adguard_registry + "filter_14_Annoyances/filter.txt",
+        "easyprivacy": "https://filters.adtidy.org/extension/ublock"
+        + "/filters/118_optimized.txt",
+    }
 
+    for name, url in urls.items():
+        rules_list = get_rules(url)
+        rules_list = remove_domains(rules_list)
+        rules_list = remove_duplicates(rules_list)
+        rules_list = sort_list(rules_list)
+        save_rules(name, rules_list)
 
-        # sort the list
-        new_rules_list.sort()
-
-
-        with open(f"without_domains/{name}.txt", "w", encoding="utf8") as f:
-            f.write("\n".join(new_rules_list))
+if __name__ == "__main__":
+    main()
